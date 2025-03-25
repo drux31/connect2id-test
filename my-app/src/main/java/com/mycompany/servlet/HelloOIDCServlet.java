4927963f250a721532c2dd04eb83b232bd2de74e
Landry N. <landry.ntsama@devops.ncia.nato.int>
2025-03-25 10:50:13 +0000

    running jetty maven plugin

---
+++ my-app/src/main/java/com/mycompany/servlet/HelloOIDCServlet.java
@@ -0,0 +1,17 @@
+package com.mycompany.servlet;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HelloOIDCServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("<h1>Hello Servlet</h1>");
+        response.getWriter().println("session=" + request.getSession(true).getId());
+    }
+}
